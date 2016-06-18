949a923cbb40f37fe1bc52010013c68b0496eea2
zeeshan <zeeshanansari38244@gmail.com>
2016-06-17 19:00:15 -0700

    not complete

---
+++ app/src/main/java/com/example/xesshan/fragments/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.xesshan.fragments;
+
+import android.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.text.Layout;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
